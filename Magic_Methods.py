5a59f008e36237af252a3a8e2835431e363f5403
Ahmed Anmazir <kyracnh>
2024-10-10 15:33:04 +0100

    Exercise 2: Magic Methods (str and repr) Instructions

---
+++ Magic_Methods.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+    
+    def __str__(self):
+        return f"'{self.title}' by {self.author}, {self.pages} pages"
+    
+    def __repr__(self):
+        return f"Book(title='{self.title}', author='{self.author}', pages='{self.pages})'"
+    
+book1 = Book("1984", "George Orwell", 328)
+book2 = Book("The pathway to success", "unknown", 281)
+
+print(str(book1))
+print(str(book2))
