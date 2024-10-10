ca3a8ef0a41f8cbf101e8dd0e7cd7a2e756c0e68
Ahmed Anmazir <kyracnh>
2024-10-10 15:21:00 +0100

    Exercise 1: Constructors and Destructors Instructions

---
+++ Constructors_Destructors.py
@@ -0,0 +1,23 @@
+class person:
+    # Constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print(f"Person {self.name} is created.")
+    
+    # Destructor
+
+    def __del__(self):
+        print(f"Person {self.name} is deleted.")
+    
+# Usage
+
+per1 = person("Kyra", 20)
+per2 = person("salim", 34)
+
+print(per1)
+
+# Deleting the objects
+
+del per1
+del per2
